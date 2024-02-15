def4504c3d774940242baf806441f84aa62ef55e
userabhirami <abhiramisurendran11@gmail.com>
2024-02-15 11:08:44 +0530

    commit

--- lib/view/loginPage.dart
+++ lib/view/loginPage.dart
@@ -98,7 +98,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     if (emailAddress.text.trim() == email &&
                         password.text.trim() == pass) {
                       await pref.setBool("isLogged", true);
-                      Navigator.pushReplacement(
+                      Navigator.push(
                         context,
                         MaterialPageRoute(
                           builder: (context) => BottomNavigation(),
@@ -123,7 +123,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   )),
               InkWell(
                 onTap: () {
-                  Navigator.push(
+                  Navigator.pushReplacement(
                       context,
                       MaterialPageRoute(
                         builder: (context) => RegisterScreen(),
--- lib/view/termsAndConditions/termsAndCondtions.dart
+++ lib/view/termsAndConditions/termsAndCondtions.dart
@@ -16,7 +16,7 @@ class TermsAndCondtions extends StatelessWidget {
           fontSize: 20,
         ),
       ),
-      body: Padding(
+      body: /* Padding(
         padding: const EdgeInsets.all(10),
         child: SingleChildScrollView(
           child: SizedBox(
@@ -25,6 +25,27 @@ class TermsAndCondtions extends StatelessWidget {
               textAlign: TextAlign.justify,
               style: TextStyle(color: Colors.black, fontSize: 18),
             ),
+          ),
+        ),
+      ), */
+          SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  '''Terms and Conditions for Waste Management App \n 1. Acceptance of Terms \n By accessing or using the Waste Management App, you agree to comply with and be bound by these Terms and Conditions. If you do not agree with any part of these terms, you may not use the app. \n 2. User Responsibilities \n You are responsible for the proper disposal of waste in accordance with local regulations. The app provides information and tools, but ultimate responsibility lies with the user. \n 3. Data Privacy \n We collect and process user data in accordance with our Privacy Policy. By using the app, you consent to the collection and use of your information as described in the Privacy Policy. \n 4. App Content \n All content provided by the app is for informational purposes only. We do not guarantee the accuracy, completeness, or usefulness of any information provided. \n 5. Prohibited Activities \n Users are prohibited from engaging in any unlawful or harmful activities while using the app. This includes but is not limited to the dissemination of harmful content, hacking, or any other illegal activities. \n 6. Intellectual Property \n The app and its original content, features, and functionality are owned by our company and are protected by international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws. \n 7. Updates and Changes \n We reserve the right to modify or update these Terms and Conditions at any time without prior notice. Continued use of the app after any changes shall constitute your consent to such changes. \n 8. Termination \n We reserve the right to terminate or suspend your access to the app immediately, without prior notice or liability, for any reason, including breach of these Terms and Conditions. \n 9. Limitation of Liability \n In no event shall our company be liable for any indirect, incidental, special, consequential, or punitive damages, or any loss of profits or revenues, whether incurred directly or indirectly. \n 10. Governing Law \n These Terms and Conditions shall be governed by and construed in accordance with the laws of our jurisdiction, without regard to its conflict of law principles. \n By using the Waste Management App, you acknowledge that you have read, understood, and agree to be bound by these Terms and Conditions.
+      ''',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                      fontSize: 18,
+                      // fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+              )
+            ],
           ),
         ),
       ),
